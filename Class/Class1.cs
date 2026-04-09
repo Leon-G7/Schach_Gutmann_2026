0d023cb2c6afa972bd40320193ad94ab0a415d7d
Student <foobar@sr.htlweiz.at>
2026-04-09 14:35:34 +0200

    ChessGame_DataStructure_and_Classes

---
+++ Class/Class1.cs
@@ -0,0 +1,59 @@
+﻿namespace ChessGame;
+
+public class GameField
+{
+
+}
+
+public class SetFigure
+{
+}
+
+public enum PieceColor
+{
+    white,
+    black
+}
+
+public enum ChessFigure
+{
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn
+}
+
+public class Piece
+{
+
+}
+
+public class King : Piece
+{
+}
+
+public class Queen : Piece
+{
+}
+
+public class Rook : Piece
+{
+}
+
+public class Bishop : Piece
+{
+}
+
+public class Knight : Piece
+{
+}
+
+public class Pawn : Piece
+{
+}
+
+public class WinCondition
+{
+}
